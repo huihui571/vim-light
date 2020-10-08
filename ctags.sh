3eef1e04e760993101821515f3e9625c4169fced
huihui571 <weina_h@foxmail.com>
2020-10-08 15:08:10 +0800

    更新ctags脚本

--- ctags.sh
+++ ctags.sh
@@ -1,11 +1,18 @@
-echo "rm all tags files..."
-rm -f tags cscope.*
+#------------------------------------------------------------------
+#v1:2019.10.14   create files
+#v2:2020.10.8    add help info and update ctags cmd
+#------------------------------------------------------------------
 
+#update for universal ctags
 function buildctags() {
-    ctags -R --c++-kinds=+px --fields=+ilaS --extra=+q
+    rm -f tags
+    echo "old tags files removed, tags building..."
+    ctags -R --c-kinds=+px --c++-kinds=+px --fields=+nilazS --extras=+q
 }
 
 function buildcscope() {
+    rm -f cscope.*
+    echo "old cscope files removed, cscope building..."
     find -name "*.py" -o -name "*.[ch]" -o -name "*.[ch]pp" > cscope.files
     if [ "$1" == "-q" ]; then
         cscope -Rbqk
@@ -35,4 +42,13 @@ elif [ "${cmd}" == "-a" ]; then
 elif [ "${cmd}" == "-c" ]; then
     rm -f tags cscope.*
     echo "all tags files cleared!"
+elif [ "${cmd}" == "-h" ]; then
+    echo "first args: 
+    -t : ctags(default)
+    -s : cscope
+    -a : both ctags and cscope
+    -c : clear all tags
+    -h : for help info"
+    echo "second args: 
+    -q : cscope quick files"
 fi
